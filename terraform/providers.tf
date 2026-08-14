1963d6bc5ddaaf26f4df555afd84cc0910c81ef1
alok8700-web <alokbind021@gmail.com>
2026-08-14 14:33:08 +0530

    feat(terraform): configure AWS EKS Kubernetes and Helm providers

---
+++ terraform/providers.tf
@@ -0,0 +1,17 @@
+provider "aws" {
+  region = var.aws_region
+}
+
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.novapay.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.novapay.certificate_authority[0].data)
+  token                  = data.aws_eks_cluster_auth.novapay.token
+}
+
+provider "helm" {
+  kubernetes = {
+    host                   = data.aws_eks_cluster.novapay.endpoint
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.novapay.certificate_authority[0].data)
+    token                  = data.aws_eks_cluster_auth.novapay.token
+  }
+}
